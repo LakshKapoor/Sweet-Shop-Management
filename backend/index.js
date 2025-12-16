f5ad1e54b85f8e51fa6f9ae4cd08af8da0940519
laksh kapoor <lakshiskapoor720@gmail.com>
2025-12-16 19:48:15 +0530

    test: add login tests; feat: implement mock login API

--- backend/index.js
+++ backend/index.js
@@ -23,3 +23,18 @@ app.post("/api/auth/register", (req, res)=>{
 
     return res.status(201).json({message: "User registered successfully (mock)"});
 })
+
+app.post("/api/auth/login", (req, res)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message: "Email and password Required"});
+    }
+
+    if(email !=="test@example.com" || password !== "password123"){
+        return res.status(401).json({message:"Invalid Credentials"});
+    }
+
+    return res.status(200).json({message:"Login successful (mock)"});
+
+});
--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -6,4 +6,41 @@ describe("User Registration", () => {
         const res = await request(app).post("/api/auth/register").send({});
         expect(res.status).toBe(400);
     });
+
+    it("should succeed when email and password are provided", async () => {
+        const res = await request(app).post("/api/auth/register")
+        .send({email: "test@example.com", password: "password123"
+    
+        });
+    
+         expect(res.status).toBe(201);
+    
+        expect(res.body.message).toBe("User registered successfully (mock)");
+    });
+});
+
+describe("User Login", () => {
+    it("should fail when email or password is missing",async ()=>{
+        const res = await request(app).post("/api/auth/login").send({    
+        });
+        expect(res.status).toBe(400)
+    });
+
+    it("should fail when credentials are invalid",async ()=>{
+        const res = await request(app).post("/api/auth/login")
+        .send({email: "wrong@example.com", password: "wrongpassword"
+
+        });
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe("Invalid Credentials");
+});
+
+it("should succeed when credentials are valid",async ()=>{
+    const res = await request(app).post("/api/auth/login")
+    .send({email: "test@example.com", password: "password123"
+
+    });
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Login successful (mock)");
+});
 });

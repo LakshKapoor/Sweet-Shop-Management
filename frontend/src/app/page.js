532473e13d0e007fc21b04da2e22e1dc89d242eb
laksh kapoor <lakshiskapoor720@gmail.com>
2025-12-27 15:30:37 +0530

    frontend: Add expense form and POST integration

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,23 +1,89 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useStyleRegistry } from "styled-jsx";
 
-/* 👇 API URL CONSTANT (HERE) */
-// const APIurl = "http://192.168.137.1:3000";
+/* API URL CONSTANT (HERE) */
+const APIurl = "http://172.25.9.156:3000";
 
 export default function Page() {
   const [expenses, setExpenses] = useState([]);
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:3000/api/expenses")
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+
+  //fetch expenses
+  const fetchExpenses=() => {
+    fetch(`${APIurl}/api/expenses`)
       .then((res) => res.json())
       .then((data) => setExpenses(data))
       .catch((err) => console.error(err));
-  }, []);
+  };
+
+  //runs once page reloads
+  useEffect(()=>{
+    fetchExpenses();
+  },[]);
+
+//handle form submit
+const handleSubmit = (e) =>{
+  e.preventDefault()
+  fetch(`${APIurl}/api/expenses`,{
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      amount: Number(amount),
+      category,
+    }),
+  })
+  .then(()=>{
+    setTitle("")
+    setAmount("")
+    setCategory("")
+    fetchExpenses()
+  })
+  .catch((err)=>console.log(err))
+};
 
   return (
     <main style={{ padding: "20px" }}>
       <h1>Expense Tracker</h1>
+
+      <form onSubmit ={handleSubmit}>
+        <input
+          placeholder="Title"
+          value ={title}
+          onChange = {(e)=>setTitle(e.target.value)}
+          />
+
+        <br />
+
+        <input
+        type="number"
+          placeholder="Amount"
+          value ={amount}
+          onChange = {(e)=>setAmount(e.target.value)}
+          />
+
+        <br />
+
+        <input
+          placeholder="Category"
+          value ={category}
+          onChange = {(e)=>setCategory(e.target.value)}
+          />
+
+          <br />
+
+          <button type="submit">Add Expense</button>
+      </form>
+
+
+      <hr />
 
       {expenses.length === 0 && <p>No expenses found</p>}
 
@@ -31,3 +97,5 @@ export default function Page() {
     </main>
   );
 }
+
+
